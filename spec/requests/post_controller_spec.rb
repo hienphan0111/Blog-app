af5bf7217b6461a9f74d4a0abb3de2042bed5ad0
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-05-16 13:22:47 +0700

    use type request for resting routes

--- spec/requests/post_controller_spec.rb
+++ spec/requests/post_controller_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 require "capybara/rspec"
 
-RSpec.describe PostsController, type: :controller do
+RSpec.describe PostsController, type: :request do
 
   describe "GET /index" do
+    let(:user) { User.create!(name: "John Doe", posts_counter: 0) }
+    let(:post) { Post.create!(author_id: user.id, title: "My first post", text: "Hello World!", comments_counter: 0, likes_counter: 0) }
+
+    it "renders a successful response" do
+      get "/users/#{user.id}/posts"
+      expect(response).to be_successful
+    end
+
     it "renders the index template" do
-      user = User.create(name: "John Doe", posts_counter: 0)
-      get :index, params: { user_id: user.id }
-      expect(response).to render_template(:index)
+      get "/users/#{user.id}/posts"
+      expect(response).to render_template("posts/index")
     end
   end
 
@@ -16,19 +23,17 @@ RSpec.describe PostsController, type: :controller do
     let(:post) { Post.create!(author_id: user.id, title: "My first post", text: "Hello World!", comments_counter: 0, likes_counter: 0) }
 
     it "returns a successful response" do
-      get :show, params: { user_id: user.id, id: post.id }
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to be_successful
     end
 
     it "renders the show template" do
-      get :show, params: { user_id: user.id, id: post.id }
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to render_template('posts/detail_post')
     end
 
-    render_views
     it "returns a response body includes correct placeholder text" do
-      get :show, params: { user_id: user.id, id: post.id }
-      expect(response).to render_template('posts/detail_post')
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response.body).to include("Show specific post's detail")
     end
   end
